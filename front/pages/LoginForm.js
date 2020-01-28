fa64d9b050f5156ab65ca907c1fa5b75c00857bf
Seol12 <seole0408@naver.com>
2020-01-28 16:38:23 +0900

    add bug fix

--- front/pages/LoginForm.js
+++ front/pages/LoginForm.js
@@ -102,12 +102,12 @@ const Guidediv = styled.div`
     }
     @media only screen and (max-width: 400px) {
       width: 240px;
-      margin-left: 20px;
+      margin-left: 30px;
      
     }
     @media only screen and (max-width: 420px) {
       width: 240px;
-      margin-left: 20px;
+      margin-left: 30px;
      
     }
 
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -95,12 +95,12 @@ const Guidediv = styled.div`
     }
   @media only screen and (max-width: 400px) {
     width: 280px;
-    margin-left: 20px;
+    margin-left: 30px;
    
   }
   @media only screen and (max-width: 420px) {
     width: 280px;
-    margin-left: 20px;
+    margin-left: 30px;
     
   }
 
