99f58a43352ffe9cb6caa0934ff64705022fe923
Seol12 <seole0408@naver.com>
2021-03-03 21:39:26 +0900

    Back URL Update

--- front/config/config.js
+++ front/config/config.js
@@ -1,2 +1,2 @@
-const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.seolcat.com' : 'http://localhost:3065';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://seolecat.com:1029' : 'http://localhost:3065';
 export {backUrl};
