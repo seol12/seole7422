c84c66ec9bca836ae1e6a9a55d98d73828517e3a
Seol12 <seole0408@naver.com>
2021-04-05 22:12:43 +0900

    Seolecat Renewal -PostImages

---
+++ front/components/PostImages/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Slider} from './style';
+
+
+const PostImages = ({ images}) => {
+  
+  if(images.length === 1) {
+    return (
+      <>
+        <img src={images[0].src} />
+      </>
+    );
+  }
+  
+  return (
+    <>
+      <div>
+        <Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1} arrows={true} accessibility={true} adaptiveHeight={true}>
+          {images.map((c,i) => {
+            return (
+              <img key ={i} src={images[i].src}/>
+            );
+          })}
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+
+export default PostImages;
---
+++ front/components/PostImages/style.js
@@ -0,0 +1,37 @@
+import Slick from 'react-slick';
+import styled from 'styled-components';
+
+
+export const Slider = styled(Slick)`
+
+    margin-bottom: 30px;
+    
+    .slick-arrow {
+    
+        width: 40px;
+        height: 60px;
+ 
+    }
+
+    .slick-prev {
+    
+        left: -50px;
+        z-index: 1;
+  
+    }
+
+    .slick-prev:before,
+    .slick-next:before {
+
+        color: black;
+        font-size: 36px;
+
+    }
+
+    .slick-next {
+
+        right: -50px;
+        z-index: 1;
+    }
+
+`;
