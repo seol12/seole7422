e488fb1ab14f0d9eec4e8c36e13ad98a29224e59
Seol12 <seole0408@naver.com>
2021-04-27 19:12:06 +0900

    IMPLEMENT - usePrevstateChanged

--- front/customhooks/index.js
+++ front/customhooks/index.js
@@ -19,7 +19,6 @@ export const usePrevstateChanged = (initialValue) => {
     const handler = useCallback(() => {
 
         setter(prev => !prev);
-        
     },[]);
     return [ value, handler, setter];
     
