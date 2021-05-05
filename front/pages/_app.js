b9a022b5b2a148cc23219263107db6444faf4a71
Seol12 <seole0408@naver.com>
2021-05-05 19:43:26 +0900

    REFACTOR - _app.js

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import PropTypes from 'prop-types';
-import {Provider, useDispatch} from 'react-redux';
+import { Provider, useDispatch} from 'react-redux';
 import withRedux from 'next-redux-wrapper';
-import {createStore, compose, applyMiddleware} from 'redux';
+import { createStore, compose, applyMiddleware} from 'redux';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 import withReduxSaga from 'next-redux-saga';
@@ -11,84 +10,64 @@ import Helmet from 'react-helmet';
 import createSagaMiddleware from 'redux-saga';
 import { LOAD_USER_REQUEST } from '../reducers/user';
 import axios from 'axios';
-import {Container} from 'next/app';
+import { Container} from 'next/app';
 
 
-
-const Didhddl = ({Component,store,pageProps}) => {
+const SeoleCat = ({ Component, store, pageProps}) => {
   
-  return(
+  return (
     <Container>
-        <Provider store={store}>
-        <Helmet
-          title="seolecat"
-          htmlAttributes={{lang: 'ko'}}
-          meta={[{
-            charSet: "UTF-8",
+      <Provider store={store}>
+      <Helmet
+        title="seolecat"
+        htmlAttributes={{lang: 'ko'}}
+        meta={[{
+          charSet: "UTF-8",
           },{
             name: 'viewport',
             content:'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
-
-
           },{
             'http-equiv': 'X-UA-Compatible',
             content: 'IE=edge',
-
           },{
             name: 'description', 
             content: 'seolecat',
-
           },{
             name: 'og:description',
             content: 'seolecat',
-
           },{
             property: 'og:type',
             content: 'website',
-
           },{
             name: 'og:title',
             content: 'seolcat',
-
           },{
             property: 'og:image',
-            content: 'http://seolecat.com/favicon.ico',
-            
-          }]}
-          link={[{
-            rel: 'shortcut icon', href: '/favicon.ico',
+            content: 'http://seolecat.com/favicon.ico', 
+        }]}
+        link={[{
+          rel: 'shortcut icon', href: '/favicon.ico',
           },{
-           rel: 'stylesheet',
+            rel: 'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
           },{
             rel:'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css",
-
           },{
             rel: 'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css",
-
-          }]}
- 
-        />
-     
+        }]}
+      />
         <Layout>
-    
-        <Component {...pageProps}/>
-  
+          <Component {...pageProps}/>
         </Layout>
-        </Provider>
-        </Container>
-    );
+      </Provider>
+    </Container>
+  );
 };
 
-Didhddl.propTypes ={
-    Component: PropTypes.elementType.isRequired,
-    store: PropTypes.object.isRequired,
-    pageProps: PropTypes.object.isRequired,
-}
-
-Didhddl.getInitialProps = async (context) => {
+SeoleCat.getInitialProps = async (context) => {
+  
   const { ctx, Component } = context;
   let pageProps = {};
   const state = ctx.store.getState();
@@ -108,17 +87,20 @@ Didhddl.getInitialProps = async (context) => {
   return { pageProps };
 };
 
-const configureStore = (initialState,options)=>{
-    const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware];
-    const enhancer = process.env.NODE_ENV === 'production'
+const configureStore = (initialState, options) => {
+    
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : compose(
       applyMiddleware(...middlewares),
       !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     );
-    const store = createStore(reducer, initialState, enhancer);
-    store.sagaTask = sagaMiddleware.run(rootSaga);
-    return store;
+  const store = createStore(reducer, initialState, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
 }
-export default withRedux(configureStore)(withReduxSaga(Didhddl));
+
+
+export default withRedux(configureStore)(withReduxSaga(SeoleCat));
