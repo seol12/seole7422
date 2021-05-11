370357ae59938cae8a72d7c97ef1c9725fad7058
Seol12 <seole0408@naver.com>
2021-05-11 17:42:15 +0900

    REVISE - hashtag.js

--- back/models/hashtag.js
+++ back/models/hashtag.js
@@ -9,9 +9,9 @@ module.exports = (sequelize, DataTypes) => {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci',
     });
-    Hashtag.associate = (db) => {
-      db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag'});
-    };
-    return Hashtag;
+  Hashtag.associate = (db) => {
+    db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag'});
+  };
+  return Hashtag;
 
 };
