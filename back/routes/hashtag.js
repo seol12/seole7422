a2705e21f838f8afccb6d9816fb0a3054df8a3ea
Seol12 <seole0408@naver.com>
2021-05-11 16:44:54 +0900

    REVISE - hashtag.js

--- back/routes/hashtag.js
+++ back/routes/hashtag.js
@@ -44,7 +44,7 @@ router.get('/:tag', async (req, res, next) => {
       order: [['createdAt', 'DESC']],
       limit: parseInt(req.query.limit, 10),
     });
-    res.json(posts);
+    return res.json(posts);
   }catch(e) {
     console.error(e);
     next(e);
