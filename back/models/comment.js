1722e1e08c0d2327ca271f3c7d2185e2de99dc77
Seol12 <seole0408@naver.com>
2021-05-11 17:37:38 +0900

    REVISE - comment.js

--- back/models/comment.js
+++ back/models/comment.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
 
-    const Comment = sequelize.define('Comment', {
-      content: {
-        type: DataTypes.TEXT, 
-        allowNull: false,
-      },
+  const Comment = sequelize.define('Comment', {
+    content: {
+      type: DataTypes.TEXT, 
+      allowNull: false,
+    },
     }, {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci',
