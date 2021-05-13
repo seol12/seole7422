ee85e356eab53c14bc4109fece5b9784a8afd899
Seol12 <seole0408@naver.com>
2021-05-13 16:54:29 +0900

    REVISE - index.js

--- front/components/PostImages/index.js
+++ front/components/PostImages/index.js
@@ -15,7 +15,7 @@ const PostImages = ({ images}) => {
   return (
     <>
       <div>
-        <Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1} arrows={true} accessibility={true} adaptiveHeight={true}>
+        <Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1} arrows={true} accessibility={true} adaptiveHeight={true} >
           {images.map((v,i) => {
             return (
               <img key ={i} src={images[i].src}/>
