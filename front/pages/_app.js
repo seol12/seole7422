5aedb079856a927ca422099e51dd1327a6c82ff5
Seol12 <seole0408@naver.com>
2021-05-14 19:03:14 +0900

    UPDATE - Use to favicon.ico

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -23,37 +23,37 @@ const SeoleCat = ({ Component, store, pageProps}) => {
         htmlAttributes={{lang: 'ko'}}
         meta={[{
           charSet: "UTF-8",
-          },{
+          }, {
             name: 'viewport',
             content:'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
-          },{
+          }, {
             'http-equiv': 'X-UA-Compatible',
             content: 'IE=edge',
-          },{
+          }, {
             name: 'description', 
             content: 'seolecat',
-          },{
+          }, {
             name: 'og:description',
             content: 'seolecat',
-          },{
+          }, {
             property: 'og:type',
             content: 'website',
-          },{
+          }, {
             name: 'og:title',
             content: 'seolcat',
-          },{
+          }, {
             property: 'og:image',
             content: 'http://seolecat.com/favicon.ico', 
         }]}
         link={[{
           rel: 'shortcut icon', href: '/favicon.ico',
-          },{
+          }, {
             rel: 'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
-          },{
+          }, {
             rel:'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css",
-          },{
+          }, {
             rel: 'stylesheet',
             href:"https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css",
         }]}
