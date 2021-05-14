647d8f8017e93393b106b0719be3185cfd9b6825
Seol12 <seole0408@naver.com>
2021-05-14 17:38:03 +0900

    REFACTOR - hashtag.js

--- front/pages/hashtag.js
+++ front/pages/hashtag.js
@@ -1,16 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { LOAD_HASHTAG_POSTS_REQUEST } from '../reducers/post';
+import React, { useCallback, useEffect} from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { LOAD_HASHTAG_POSTS_REQUEST} from '../reducers/post';
 import PostFrame from '../containers/PostFrame';
 
-const Hashtag = ({ tag }) => {
-  const dispatch = useDispatch();
 
-  const { mainPosts, hasMorePost } = useSelector(state => state.post);
+const Hashtag = ({ tag}) => {
+
+  const dispatch = useDispatch();
+  const { mainPosts, hasMorePost} = useSelector(state => state.post);
+
 
   const onScroll = useCallback(() => {
-    if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
-      if (hasMorePost) {
+
+    if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+      if(hasMorePost) {
         dispatch({
           type: LOAD_HASHTAG_POSTS_REQUEST,
           lastId: mainPosts[mainPosts.length - 1] && mainPosts[mainPosts.length - 1].id,
@@ -18,32 +21,30 @@ const Hashtag = ({ tag }) => {
         });
       }
     }
-  }, [hasMorePost, mainPosts.length]);
+
+  },[ hasMorePost, mainPosts.length]);
 
   useEffect(() => {
+
     window.addEventListener('scroll', onScroll);
     return () => {
       window.removeEventListener('scroll', onScroll);
     };
-  }, [mainPosts.length]);
+
+  },[mainPosts.length]);
+
 
   return (
     <div>
-      {mainPosts.map(c => (
-        <PostFrame key={c.id} post={c} />
-      ))}
+      {mainPosts.map((v) => {
+        return (
+          <PostFrame key={v.id} post={v} />
+        );
+      })}
     </div>
   );
+  
 };
 
-
-Hashtag.getInitialProps = async (context) => {
-  const tag = context.query.tag;
-  context.store.dispatch({
-    type: LOAD_HASHTAG_POSTS_REQUEST,
-    data: tag,
-  });
-  return { tag };
-};
 
 export default Hashtag;
