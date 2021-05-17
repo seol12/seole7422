3f88f72632f55a5f58125db8e11e9e925ca0e169
Seol12 <seole0408@naver.com>
2021-05-17 18:34:25 +0900

    REFACTOR - _document.js

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,44 +1,40 @@
 import React from 'react';
-import Document, {Main, NextScript}  from 'next/document';
+import Document, { Main, NextScript}  from 'next/document';
 import Helmet from 'react-helmet';
-import PropTypes from 'prop-types';
-
-import {ServerStyleSheet} from 'styled-components';
+import { ServerStyleSheet} from 'styled-components';
 
 
-class Mydocument extends Document{
-    static getInitialProps(context) {
+class Mydocument extends Document {
+
+  static getInitialProps(context) {
     const sheet = new ServerStyleSheet();
-      const page = context.renderPage((App) => (props) =>sheet.collectStyles(  <App {...props}/>));
-      const styleTags = sheet.getStyleElement();
-        return {...page, helmet: Helmet.renderStatic(), styleTags};
+    const page = context.renderPage((App) => (props) => sheet.collectStyles(  <App {...props}/>));
+    const styleTags = sheet.getStyleElement();
+    return {...page, helmet: Helmet.renderStatic(), styleTags};
 
-    }
-    render(){
-        const {htmlAttributes, bodyAttributes, ...helmet} = this.props.helmet;
-        const htmlAttrs = htmlAttributes.toComponent();
-        const bodyAttrs = bodyAttributes.toComponent();
-        return(
-            <html {...htmlAttrs}>
-                <head>
-                {this.props.styleTags}
-                {Object.values(helmet).map(el => el.toComponent())}
-
-                </head>
-                <body {...bodyAttrs}>
-                    <Main/>
-                    {process.env.NODE_ENV === 'production'
-                     && <script src="https://polyfill.io/v3/polyfill.min.js?features=es6,es7,es8,es9,NodeList.prototype.forEach&flags=gated" />}
-                    <NextScript/>
-                </body>
-            </html>
-        );
-    }
-}
-
-Mydocument.propTypes={
-    helmet: PropTypes.object.isRequired,
-    styleTags: PropTypes.object.isRequired,
+  }
+  render() {
+    const { htmlAttributes, bodyAttributes, ...helmet} = this.props.helmet;
+    const htmlAttrs = htmlAttributes.toComponent();
+    const bodyAttrs = bodyAttributes.toComponent();
+    return (
+      <html {...htmlAttrs}>
+        <head>
+          {this.props.styleTags}
+          {Object.values(helmet).map(v => v.toComponent())}
+        </head>
+        <body {...bodyAttrs}>
+          <Main/>
+          {process.env.NODE_ENV === 'production'
+          && <script src="https://polyfill.io/v3/polyfill.min.js?features=es6,es7,es8,es9,NodeList.prototype.forEach&flags=gated" />
+          }
+          <NextScript/>
+        </body>
+      </html>
+    );
+  }
 
 }
+
+
 export default Mydocument;
