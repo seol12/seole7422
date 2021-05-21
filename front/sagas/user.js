085e05950a645eec7ffca06438623f559047f412
Seol12 <seole0408@naver.com>
2021-05-21 19:15:05 +0900

    REVISE - user.js

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,94 +1,113 @@
-import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
+import { all, call, fork, put, takeEvery} from 'redux-saga/effects';
 import axios from 'axios';
-import { 
-  LOAD_USER_FAILURE, LOAD_USER_REQUEST, LOAD_USER_SUCCESS,
-  LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS,
-  LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, 
-  SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS,
+import { LOAD_USER_FAILURE, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS,
+LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS,
 } from '../reducers/user';
 
+
 function logInAPI(loginData) {
+
   return axios.post('/user/login', loginData, {
     withCredentials: true,
   });
+
 }
 
 function* logIn(action) {
+
   try {
     const result = yield call(logInAPI, action.data);
     yield put({ 
       type: LOG_IN_SUCCESS,
       data: result.data,
     });
-  } catch (e) { 
+  }catch(e) { 
     console.error(e);
     yield put({
       type: LOG_IN_FAILURE,
       reason: e.response && e.response.data,
     });
   }
+
 }
 
 function* watchLogIn() {
+
   yield takeEvery(LOG_IN_REQUEST, logIn);
+
 }
 
 function signUpAPI(signUpData) {
+
   return axios.post('/user/', signUpData);
+
 }
 
 function* signUp(action) {
+
   try {
     const result = yield call(signUpAPI, action.data);
     yield put({ 
       type: SIGN_UP_SUCCESS,
       data: result.data,
     });
-  } catch (e) { 
+  }catch(e) { 
     console.error(e);
     yield put({
       type: SIGN_UP_FAILURE,
       error: e.response.data,
     });
   }
+
 }
 
 function* watchSignUp() {
+
   yield takeEvery(SIGN_UP_REQUEST, signUp);
+
 }
 
 function logOutAPI() {
+
   return axios.post('/user/logout', {}, {
     withCredentials: true,
   });
+
 }
 
 function* logOut() {
+
   try {
     yield call(logOutAPI);
     yield put({ 
       type: LOG_OUT_SUCCESS,
     });
-  } catch (e) { 
+  }catch(e) { 
     console.error(e);
     yield put({
       type: LOG_OUT_FAILURE,
       error: e,
     });
   }
+
 }
 
 function* watchLogOut() {
+
   yield takeEvery(LOG_OUT_REQUEST, logOut);
+
 }
 
 function loadUserAPI(userId) {
+
   return axios.get(userId ? `/user/${userId}` : '/user/', {
     withCredentials: true, 
   }); 
+
 }
 
 function* loadUser(action) {
+
   try {
     const result = yield call(loadUserAPI, action.data);
     yield put({ 
@@ -96,24 +115,29 @@ function* loadUser(action) {
       data: result.data,
       me: !action.data,
     });
-  } catch (e) { 
+  }catch(e) { 
     console.error(e);
     yield put({
       type: LOAD_USER_FAILURE,
       error: e,
     });
   }
+
 }
 
 function* watchLoadUser() {
+
   yield takeEvery(LOAD_USER_REQUEST, loadUser);
+
 }
 
 export default function* userSaga() {
+
   yield all([
     fork(watchLogIn),
     fork(watchLogOut),
     fork(watchLoadUser),
     fork(watchSignUp),
   ]);
+  
 }
