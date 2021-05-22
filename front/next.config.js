12df8ccf1fd27842f18f733c73a4645967d2059f
Seol12 <seole0408@naver.com>
2021-05-22 18:41:46 +0900

    REVISE - next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -6,44 +6,39 @@ const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 
-module.exports = withBundelAnalyzer ( {
-  webpack(config){
-  const produc = process.env.NODE_ENV === 'production';
+module.exports = withBundelAnalyzer({
 
-  const plugins =[
+  webpack(config) {
+    const produce = process.env.NODE_ENV === 'production';
+    const plugins = [
       ...config.plugins,
       new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
-  ];
-  if(produc){
-      plugins.push( new CompressionPlugin())
-  }
-  
-  config.module.rules.push({
-    test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
-    use:[{
+    ];
+    if(produce) {
+      plugins.push(new CompressionPlugin());
+    }
+    config.module.rules.push({
+      test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+      use:[{
         loader: "url-loader",
-        options:{
-            limit:10000,
-            fallback: "file-loader",
-            name: "static/[name].[ext]",
-            esModule: false,
-         
+        options: {
+          limit:10000,
+          fallback: "file-loader",
+          name: "static/[name].[ext]",
+          esModule: false,
         },
-    }],
-  });
-
-  return {
+      }],
+    });
+    return {
       ...config,
-      mode: produc ? 'production' : 'development',
-      devtool: produc  ? 'hidden-source-map' : 'eval',
-      module:{
-      ...config.module,
-      
-    
+      mode: produce ? 'production' : 'development',
+      devtool: produce  ? 'hidden-source-map' : 'eval',
+      module: {
+        ...config.module,
       },
       plugins,
-
-  }
-},
+    }
+  },
 
 });
+
