42efdec9c02999ec83dd992e6be38b093495af45
Seol12 <seole0408@naver.com>
2021-05-24 19:18:14 +0900

    ADD -  CommentModels for user.js

--- back/routes/user.js
+++ back/routes/user.js
@@ -2,23 +2,28 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const db = require('../models');
-const { isLoggedIn } = require('./middleware');
+const { isLoggedIn} = require('./middleware');
 
 const router = express.Router();
 
+
 router.get('/', isLoggedIn, (req, res) => { 
+
   const user = Object.assign({}, req.user.toJSON());
   delete user.password;
   return res.json(user);
+
 });
+
 router.post('/', async (req, res, next) => { 
+
   try {
     const exUser = await db.User.findOne({
       where: {
         userId: req.body.userId,
       },
     });
-    if (exUser) {
+    if(exUser) {
       return res.status(403).send('이미 사용중인 아이디입니다.');
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 12); 
@@ -28,13 +33,15 @@ router.post('/', async (req, res, next) => {
       password: hashedPassword,
     });
     return res.status(200).send(newUser.userId);
-  } catch (e) {
+  }catch(e) {
     console.error(e);
     return next(e);
   }
+
 });
 
 router.get('/:id', async (req, res, next) => { 
+
   try {
     const user = await db.User.findOne({
       where: { id: parseInt(req.params.id, 10) },
@@ -48,30 +55,34 @@ router.get('/:id', async (req, res, next) => {
     const jsonUser = user.toJSON();
     jsonUser.Posts = jsonUser.Posts ? jsonUser.Posts.length : 0;
     res.json(jsonUser);
-  } catch (e) {
+  }catch(e) {
     console.error(e);
     next(e);
   }
+
 });
 
-router.post('/logout', (req, res) => { 
+router.post('/logout', (req, res) => {
+
   req.logout();
   req.session.destroy();
-  res.send('logout 성공');
+  res.send('로그아웃 완료');
+
 });
 
-router.post('/login', (req, res, next) => { 
+router.post('/login', (req, res, next) => {
+
   passport.authenticate('local', (err, user, info) => {
-    if (err) {
+    if(err) {
       console.error(err);
       return next(err);
     }
-    if (info) {
+    if(info) {
       return res.status(401).send(info.reason);
     }
     return req.login(user, async (loginErr) => {
       try {
-        if (loginErr) {
+        if(loginErr) {
           return next(loginErr);
         }
         const fullUser = await db.User.findOne({
@@ -85,14 +96,16 @@ router.post('/login', (req, res, next) => {
         });
         console.log(fullUser);
         return res.json(fullUser);
-      } catch (e) {
+      }catch(e) {
         next(e);
       }
     });
   })(req, res, next);
+
 });
 
 router.get('/:id/posts', async (req, res, next) => {
+
   try {
     const selectuser = await db.User.findOne({
       where: { 
@@ -113,13 +126,20 @@ router.get('/:id/posts', async (req, res, next) => {
         through: 'Like',
         as: 'Likers',
         attributes: ['id'],
+      }, {
+        model: db.Comment,
+        include: [{
+          model: db.User,
+          attributes: ['id', 'nickname'],
+        }],
       }],
     })
     return res.json(userposts);
-  } catch (e) {
+  }catch(e) {
     console.error(e);
     next(e);
   }
+
 });
 
 
