a990a0deeb115e7030ac14e16492e8b25d4cd0dc
Seol12 <seole0408@naver.com>
2021-05-24 18:53:52 +0900

    REVISE - index.js

--- front/customhooks/index.js
+++ front/customhooks/index.js
@@ -3,24 +3,20 @@ import React, { useCallback, useState } from 'react';
 
 export const useValueChanged = (initialValue = null) => {
 
-    const [ value, setter] = useState(initialValue);
-    const handler = useCallback((e) => {
-    
-        setter(e.target.value);
-
-    },[]);
-    return [value, handler, setter];
+  const [ value, setter] = useState(initialValue);
+  const handler = useCallback((e) => {
+    setter(e.target.value);
+  },[]);
+  return [value, handler, setter];
 
 }
 
 export const usePrevstateChanged = (initialValue) => {
 
-    const [ value, setter] = useState(initialValue);
-    const handler = useCallback(() => {
-
-        setter(prev => !prev);
-        
-    },[]);
-    return [ value, handler, setter];
+  const [ value, setter] = useState(initialValue);
+  const handler = useCallback(() => {
+    setter(prev => !prev);
+  },[]);
+  return [ value, handler, setter];
     
 }
