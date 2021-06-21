044b525a45c22fb64176e084cfa6dce683eef0a0
Seol12 <seole0408@naver.com>
2021-06-21 15:39:57 +0900

    IMPLEMENT - ViewUserPosts Component

---
+++ front/components/ViewUserPosts/index.js
@@ -0,0 +1,19 @@
+import React, { forwardRef} from 'react';
+import Link from 'next/link';
+
+
+const ViewUserPosts = () => {
+
+  return (
+    <>
+    <ToggleWrapper ref={toggleRef}>
+      <UserPosts>
+        <span><Link href={{pathname: '/UserPosts', query: {id: post.User.id} }} as={`/UserPosts/${post.User.id}`}><a>작성 글 보기</a></Link></span>
+      </UserPosts>    
+    </ToggleWrapper>
+    </>
+  );
+
+}
+
+export default forwardRef(ViewUserPosts);
