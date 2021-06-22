95950e596499b450487eb5513a3dd4f3814fc83d
Seol12 <seole0408@naver.com>
2021-06-22 17:36:20 +0900

    USE - ViewUserPosts for index.js

--- front/containers/CommentList/index.js
+++ front/containers/CommentList/index.js
@@ -1,23 +1,42 @@
-import React, { useState, useCallback} from 'react';
+import React, { useState, useCallback, useRef, useEffect} from 'react';
 import { useSelector, useDispatch,  } from 'react-redux';
 import Link from 'next/link';
 import { Tooltip} from 'antd';
-import { CommentWrapper, CommentBody, NickName, AvataWrapper, AvataContent, CreationDate, 
-CommentContent, RemoveCommentWrapper, RemoveCommentButton,
-} from './style';
+import { CommentWrapper, CommentHead, AvatarWrapper, AvatarContent, ContentWrapper, NicknameWrapper, 
+Nickname, CreationDate, CommentBody, AvatarBorderLine, CommentBorderLine, CommentContent, 
+RemoveCommentWrapper, RemoveCommentButton,} from './style';
 import { REMOVE_COMMENT_REQUEST} from '../../reducers/post';
 import moment from 'moment';
 import { usePrevstateChanged} from '../../customhooks';
 import Modal from '../../components/Modal';
+import ViewUserPosts from '../../components/ViewUserPosts';
 moment.locale('ko');
 
 
 const CommentList = ({ comments, post}) => {
   
   const [ CommentModalon, OnToggleCommentModal] = usePrevstateChanged(false);
+  const [ commentToggleon, setCommentToggleOn] = useState(false);
   const id = useSelector(state => state.user.me && state.user.me.id);
   const dispatch = useDispatch();
+  const commenttoggleref = useRef();
   
+
+  useEffect(() => {
+      
+    const handleClickOutside = (e) => {
+      if (commentToggleon && (commenttoggleref.current && !commenttoggleref.current.contains(e.target))) {
+        e.stopPropagation();
+        setCommentToggleOn(false);
+      }
+      e.stopPropagation();
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+
+  }, [commentToggleon]);
 
   const onRemoveComment = useCallback((itemId) => (e) => {
     
@@ -33,30 +52,43 @@ const CommentList = ({ comments, post}) => {
           
   },[])
 
+  const viewuserpostsToggleOn = useCallback(() => {
+
+    setCommentToggleOn(true);  
+
+  },[])
+
 
   return (
     <>
       <CommentWrapper>
-        <CommentBody             
-          author={<NickName><p>{comments.User.nickname}</p></NickName>}
-          avatar={<Link href={{pathname: '/user', query: {id: comments.User.id}}}
-          as={`/user/${comments.User.id}`} ><AvataWrapper><a><AvataContent>{comments.User.nickname[0]}</AvataContent></a></AvataWrapper></Link> }
-          datetime={
-            <>
+        <CommentHead>
+          <AvatarWrapper>
+            <AvatarContent><p>{comments.User.nickname[0]}</p></AvatarContent>
+          </AvatarWrapper>
+          <ContentWrapper>
+            <NicknameWrapper>
+              <Nickname><p onClick={viewuserpostsToggleOn}>{comments.User.nickname}</p></Nickname>
               <Tooltip title={moment(comments.createdAt).format('YYYY.MM.DD HH:mm:ss')}>
                 <CreationDate>{moment(comments.createdAt).fromNow()}</CreationDate>
               </Tooltip>
-            </>
-          }
-          content={<CommentContent>{comments.content}</CommentContent>}
-        />
-        {id && comments.User.id === id                
+              {commentToggleon && <ViewUserPosts post={post} ref={commenttoggleref}/>}
+            </NicknameWrapper>
+          </ContentWrapper>
+          {id && comments.User.id === id                
           ?<RemoveCommentWrapper>
             <RemoveCommentButton onClick={OnToggleCommentModal}>제거</RemoveCommentButton>
               {CommentModalon && (<Modal post={post} item={comments.id} onSub={onRemoveComment(comments.id)} onClose={OnToggleCommentModal}/>)} 
             </RemoveCommentWrapper>
           :null
-        }
+          }
+        </CommentHead>
+        <CommentBody>
+          <AvatarBorderLine/>
+          <CommentBorderLine>
+            <CommentContent>{comments.content}</CommentContent>
+          </CommentBorderLine>
+        </CommentBody>
       </CommentWrapper>          
     </>
   );
