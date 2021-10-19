8cd79e3685d25b0a47d026025976b9a3a632fab3
Seol12 <seole0408@naver.com>
2021-10-19 18:07:15 +0900

    ADD - Styled Component For PostContent.js

---
+++ front/components/PostContent/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const VideoWrap = styled.div`
+  
+  position: relative; 
+  height: 0; 
+  overflow: hidden; 
+  padding-bottom: 56.25%; 
+  padding-top: 30px;
+
+`;
+
+export const Videoiframe = styled.iframe`
+
+  position: absolute; 
+  width: 100%; 
+  height: 100%;
+  top: 0; 
+  left: 0; 
+  border: none;
+
+`;
