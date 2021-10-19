4f47823a661ace7b12acc906892b6eaf10cce275
Seol12 <seole0408@naver.com>
2021-10-19 18:25:58 +0900

    IMPLEMENT - YouTube Player and UrlLink

--- front/components/PostContent/index.js
+++ front/components/PostContent/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import { VideoWrapper, Video} from './style';
 
 
 const PostContent = ({ postData}) => {
     
   return (
     <div>
-      {postData.split(/(#[^\s]+)/g).map((v) => {
-        if(v.match(/#[^\s]+/)) {
+      {postData.split(/(https?:[^\s]+)/g).map((v) => {
+        if(v.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/)) {
+          let urlkeyvalue = v.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/)[7];
           return (
-            <Link
-              href={{ pathname: '/hashtag', query: { tag: v.slice(1)}}}
-              as={`/hashtag/${v.slice(1)}`}
-              key={v}
-            >
-              <a>{v}</a>
-            </Link>
+            <div>
+              <VideoWrapper>
+                <Video src={`https://www.youtube.com/embed/${urlkeyvalue}`} title="YouTube video player" 
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+                </Video>
+              </VideoWrapper>
+            </div>
+          );
+        }
+        if(v.match(/(http(s)?:\/\/)([a-z 0-9\w]+\.*)+([a-z 0-9]{2,4})/)) {
+          return (
+            <Link><a href={v}>{v}</a></Link>
           );
         }
         return v;
